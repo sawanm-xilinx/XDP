7636f404a9ec9ae77a9e4aaa1b2fdbc82190216d
Paul Schumacher <paul.schumacher@amd.com>
2026-04-10 12:02:35 -0600

    temp use of constants for npu3 halt plugin
    Signed-off-by: Paul Schumacher <paul.schumacher@amd.com>

--- profile/plugin/aie_halt/clientDev/aie_halt_npu3.cpp
+++ profile/plugin/aie_halt/clientDev/aie_halt_npu3.cpp
@@ -98,7 +98,7 @@ namespace xdp {
     xrt_core::message::send(xrt_core::message::severity_level::debug, "XRT",
         "Creating AIE Halt control code");
 
-    // Configure driver
+#if 0
     const xdp::aie::BaseFiletypeImpl *metadataReader = 
       (db->getStaticInfo()).getAIEmetadataReader(mDeviceId);
     if (!metadataReader) {
@@ -108,6 +108,21 @@ namespace xdp {
     }
 
     auto meta_config = metadataReader->getDriverConfig();
+#else
+    // Hardcoded NPU3 (AIE4/Medusa) driver config constants
+    xdp::aie::driver_config meta_config {};
+    meta_config.hw_gen             = XAIE_DEV_GEN_AIE4;  // 42
+    meta_config.base_address       = 0;
+    meta_config.column_shift       = 25;
+    meta_config.row_shift          = 20;
+    meta_config.num_rows           = 6;   // 1 shim + 1 mem + 4 AIE
+    meta_config.num_columns        = 4;
+    meta_config.shim_row           = 0;
+    meta_config.mem_row_start      = 1;
+    meta_config.mem_num_rows       = 1;
+    meta_config.aie_tile_row_start = 2;
+    meta_config.aie_tile_num_rows  = 4;
+#endif
 
     XAie_Config cfg {
       meta_config.hw_gen,
